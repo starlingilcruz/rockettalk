3b088d85f01b366c08ac8e479ea3e01af34cc214
Starlin Gil Cruz <starlin.gil.cruz@gmail.com>
2023-09-29 09:53:03 -0400

    Setup postgres database

--- rockettalk/rockettalk/settings.py
+++ rockettalk/rockettalk/settings.py
@@ -100,9 +100,13 @@ LOGOUT_REDIRECT_URL = "login-user"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+       "ENGINE": 'django.db.backends.postgresql',
+       "NAME": os.environ.get("DATABASE_NAME"),
+       "USER": os.environ.get("DATABASE_USER"),
+       "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+       "HOST": os.environ.get("DATABASE_HOST"),
+       "PORT": os.environ.get("DATABASE_PORT"),
+   }
 }
 
 LOGGING = {
