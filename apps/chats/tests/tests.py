0daef2fbe3963d6dca6542d7e0e990013adb7bb3
Starlin Gil Cruz <starlin.gil.cruz@gmail.com>
2023-10-09 08:17:58 -0400

    Add tests for redis backend

--- apps/chats/tests/tests.py
+++ apps/chats/tests/tests.py
@@ -6,9 +6,9 @@ from channels.testing import WebsocketCommunicator
 from channels.db import database_sync_to_async
 from channels.routing import URLRouter
 
-from .consumers import ChatConsumer
-from .store import RedisStore
-from .utils import uuid
+from ..consumers import ChatConsumer
+from ..store import RedisStore
+from ..utils import uuid
 
 
 class ChatConsumerTest(IsolatedAsyncioTestCase):
